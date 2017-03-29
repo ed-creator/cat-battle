a5bc473a1f78877a563d72489391523d27d8792b
Edward Ward <edwardwardward@gmail.com>
2017-03-29 12:14:58 +0100

    Add player Hit Points

--- app.rb
+++ app.rb
@@ -1,13 +1,33 @@
 require "sinatra/base"
 
+
 class Battle < Sinatra::Base
+  enable :sessions
 
   get '/' do
     #p params
     #@name = params[:name]
     # erb :battle
-    'Testing infrastructure working!'
+    erb :index
+    # 'Testing infrastructure working!'
+  end
+
+  post '/names' do
+    #Params is like a big hash that stores the (user inputed) variables.
+    session[:player_1_name] = params[:player_1_name] #Session is used to store short-term info in a hash.
+    session[:player_2_name] = params[:player_2_name]
+    redirect '/play' # And remove the erb :play.
+  end
+
+  # Store the names in a session i.e. new game.
+  # Extract the names from session to play, so we can play using the names submitted.
+
+  get '/play' do
+    @player_1_name = session[:player_1_name]
+    @player_2_name = session[:player_2_name]
+    erb :play
   end
 
   run if app_file = $0
+
 end
--- spec/features/feature_spec.rb
+++ spec/features/feature_spec.rb
@@ -1,7 +1,28 @@
 # Write a feature test that checks that the hompage says Testing infrastructure working!.
-feature 'Testing infrastructure' do # Like 'Describe'.
-  scenario 'Can run app and check page content' do # Like 'it'.
-    visit('/') # Specify URL.
-    expect(page).to have_content 'Testing infrastructure working!'
-    end
+# feature 'Testing infrastructure' do # Like 'Describe'.
+#   scenario 'Can run app and check page content' do # Like 'it'.
+#     visit('/') # Specify URL.
+#     expect(page).to have_content 'Testing infrastructure working!'
+#     end
+#   end
+
+feature 'Player Sign-up' do
+  scenario 'Player 1 can see the name they provide' do
+    visit('/')
+    fill_in :player_1_name, with: 'Ed'
+    fill_in :player_2_name, with: 'Connie'
+    click_button 'Submit'
+    expect(page).to have_content 'Ed vs. Connie'
   end
+end
+
+feature 'Display Hit Points' do
+  scenario 'Players can see each others hit points' do
+    visit('/')
+    fill_in :player_1_name, with: 'Ed'
+    fill_in :player_2_name, with: 'Connie'
+    click_button 'Submit'
+    expect(page).to have_content 'Ed: 100HP'
+    expect(page).to have_content 'Connie: 100HP'
+  end
+end
