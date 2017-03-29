481021fe7d0ceffd444232a83931886230d005ce
Edward Ward <edwardwardward@gmail.com>
2017-03-29 12:37:36 +0100

    Add attack

--- spec/features/feature_spec.rb
+++ spec/features/feature_spec.rb
@@ -8,21 +8,23 @@
 
 feature 'Player Sign-up' do
   scenario 'Player 1 can see the name they provide' do
-    visit('/')
-    fill_in :player_1_name, with: 'Ed'
-    fill_in :player_2_name, with: 'Connie'
-    click_button 'Submit'
+        sign_in_and_play
     expect(page).to have_content 'Ed vs. Connie'
   end
 end
 
 feature 'Display Hit Points' do
   scenario 'Players can see each others hit points' do
-    visit('/')
-    fill_in :player_1_name, with: 'Ed'
-    fill_in :player_2_name, with: 'Connie'
-    click_button 'Submit'
+        sign_in_and_play
     expect(page).to have_content 'Ed: 100HP'
     expect(page).to have_content 'Connie: 100HP'
+  end
+end
+
+feature 'Attack player 2' do
+  scenario 'Player 1 attacks player 2' do
+    sign_in_and_play
+    click_link 'BITE!'
+    expect(page).to have_content "Ed has used BITE!"
   end
 end
